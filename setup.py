3acd523adbbe6feaa1bb6f62ba8f592be0a441e8
hingston <will.hingston@gmail.com>
2017-12-02 04:04:21 +1030

    Version 1.0

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='wall-timeout',
+    packages=['wall-timeout'],
+    version='1.0',
+    description='A wall timeout decorator for Python.',
+    author='William Hingston',
+    author_email='will@hingston.org',
+    url='https://github.com/hingston/python-wall-timeout',
+    download_url='https://github.com/hingston/python-wall-timeout/archive/1.0.tar.gz',
+    keywords=['wall-timeout', 'wall', 'timeout'],
+    classifiers=[],
+)
